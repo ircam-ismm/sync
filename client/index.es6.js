9e738d9793b75ba49c37af2a2059e5c9fab577ca
Sébastien Robaszkiewicz <sebastien.robaszkiewicz@gmail.com>
2015-03-09 16:18:14 +0100

    sendFunction, receiveFunction, callback in the .start() method

--- client/index.es6.js
+++ client/index.es6.js
@@ -1,7 +1,7 @@
 'use strict';
 
 class SyncClient {
-  constructor(getTimeFunction, sendFunction, receiveFunction, callback, options = {}) {
+  constructor(getTimeFunction, options = {}) {
     this.pingIterations = options.pingIterations || 10; // number of ping-pongs in a streak
     this.pingInterval = options.pingInterval || 0.250; // interval between pings in a streak (in seconds)
     this.pingSleepInterval = this.pingSleepInterval || [10, 20]; // range of interval between ping-pong streaks (in seconds)
@@ -21,11 +21,29 @@ class SyncClient {
     }
 
     this.getTimeFunction = getTimeFunction;
-    this.sendFunction = sendFunction;
-    this.receiveFunction = receiveFunction;
-    this.callback = callback;
+  }
 
-    this.receiveFunction('sync_pong', (pingId, clientPingTime, serverPingTime, serverPongTime) => {
+  __syncLoop(sendFunction) {
+    let interval;
+
+    if (this.pingCount < this.pingIterations - 1) { // if we are in a streak, use the pingInterval value
+      interval = this.pingInterval;
+      ++this.pingCount;
+    } else { // if we reached the end of a streak, plan for the begining of the next streak
+      interval = this.pingSleepInterval[0] + Math.random() * (this.pingSleepInterval[1] - this.pingSleepInterval[0]);
+      this.pingCount = 0;
+    }
+    
+    ++this.pingId;
+    sendFunction('sync_ping', this.pingId, this.getTimeFunction());
+
+    setTimeout(() => {
+      this.__syncLoop();
+    }, 1000 * interval);
+  }
+
+  start(sendFunction, receiveFunction, callback) {
+    receiveFunction('sync_pong', (pingId, clientPingTime, serverPingTime, serverPongTime) => {
       if (pingId === this.pingId) {
         var clientPongTime = this.getTimeFunction();
         var travelTime = Math.max(0, (clientPongTime - clientPingTime) - (serverPongTime - serverPingTime));
@@ -39,34 +57,12 @@ class SyncClient {
           let quickest = this.data.slice(0).sort().slice(0, this.dataBest);
           this.travelTime = quickest.reduce((p, q) => p + q[0], 0) / quickest.length;
           this.timeOffset = quickest.reduce((p, q) => p + q[1], 0) / quickest.length;
-          this.callback(this.timeOffset);
+          callback(this.timeOffset);
         }
       }
     });
 
-  }
-
-  __syncLoop() {
-    let interval;
-
-    if (this.pingCount < this.pingIterations - 1) { // if we are in a streak, use the pingInterval value
-      interval = this.pingInterval;
-      ++this.pingCount;
-    } else { // if we reached the end of a streak, plan for the begining of the next streak
-      interval = this.pingSleepInterval[0] + Math.random() * (this.pingSleepInterval[1] - this.pingSleepInterval[0]);
-      this.pingCount = 0;
-    }
-    
-    ++this.pingId;
-    this.sendFunction('sync_ping', this.pingId, this.getTimeFunction());
-
-    setTimeout(() => {
-      this.__syncLoop();
-    }, 1000 * interval);
-  }
-
-  start() {
-    this.__syncLoop();
+    this.__syncLoop(sendFunction);
   }
 
   getLocalTime(syncTime) {
--- client/index.js
+++ client/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var SyncClient = (function(){var PRS$0 = (function(o,t){o["__proto__"]={"a":t};return o["a"]===t})({},{});var DP$0 = Object.defineProperty;var GOPD$0 = Object.getOwnPropertyDescriptor;var MIXIN$0 = function(t,s){for(var p in s){if(s.hasOwnProperty(p)){DP$0(t,p,GOPD$0(s,p));}}return t};var proto$0={};
-  function SyncClient(getTimeFunction, sendFunction, receiveFunction, callback) {var options = arguments[4];if(options === void 0)options = {};var this$0 = this;
+  function SyncClient(getTimeFunction) {var options = arguments[1];if(options === void 0)options = {};
     this.pingIterations = options.pingIterations || 10; // number of ping-pongs in a streak
     this.pingInterval = options.pingInterval || 0.250; // interval between pings in a streak (in seconds)
     this.pingSleepInterval = this.pingSleepInterval || [10, 20]; // range of interval between ping-pong streaks (in seconds)
@@ -21,11 +21,29 @@ var SyncClient = (function(){var PRS$0 = (function(o,t){o["__proto__"]={"a":t};r
     }
 
     this.getTimeFunction = getTimeFunction;
-    this.sendFunction = sendFunction;
-    this.receiveFunction = receiveFunction;
-    this.callback = callback;
+  }DP$0(SyncClient,"prototype",{"configurable":false,"enumerable":false,"writable":false});
 
-    this.receiveFunction('sync_pong', function(pingId, clientPingTime, serverPingTime, serverPongTime)  {
+  proto$0.__syncLoop = function(sendFunction) {var this$0 = this;
+    var interval;
+
+    if (this.pingCount < this.pingIterations - 1) { // if we are in a streak, use the pingInterval value
+      interval = this.pingInterval;
+      ++this.pingCount;
+    } else { // if we reached the end of a streak, plan for the begining of the next streak
+      interval = this.pingSleepInterval[0] + Math.random() * (this.pingSleepInterval[1] - this.pingSleepInterval[0]);
+      this.pingCount = 0;
+    }
+    
+    ++this.pingId;
+    sendFunction('sync_ping', this.pingId, this.getTimeFunction());
+
+    setTimeout(function()  {
+      this$0.__syncLoop();
+    }, 1000 * interval);
+  };
+
+  proto$0.start = function(sendFunction, receiveFunction, callback) {var this$0 = this;
+    receiveFunction('sync_pong', function(pingId, clientPingTime, serverPingTime, serverPongTime)  {
       if (pingId === this$0.pingId) {
         var clientPongTime = this$0.getTimeFunction();
         var travelTime = Math.max(0, (clientPongTime - clientPingTime) - (serverPongTime - serverPingTime));
@@ -39,34 +57,12 @@ var SyncClient = (function(){var PRS$0 = (function(o,t){o["__proto__"]={"a":t};r
           var quickest = this$0.data.slice(0).sort().slice(0, this$0.dataBest);
           this$0.travelTime = quickest.reduce(function(p, q)  {return p + q[0]}, 0) / quickest.length;
           this$0.timeOffset = quickest.reduce(function(p, q)  {return p + q[1]}, 0) / quickest.length;
-          this$0.callback(this$0.timeOffset);
+          callback(this$0.timeOffset);
         }
       }
     });
 
-  }DP$0(SyncClient,"prototype",{"configurable":false,"enumerable":false,"writable":false});
-
-  proto$0.__syncLoop = function() {var this$0 = this;
-    var interval;
-
-    if (this.pingCount < this.pingIterations - 1) { // if we are in a streak, use the pingInterval value
-      interval = this.pingInterval;
-      ++this.pingCount;
-    } else { // if we reached the end of a streak, plan for the begining of the next streak
-      interval = this.pingSleepInterval[0] + Math.random() * (this.pingSleepInterval[1] - this.pingSleepInterval[0]);
-      this.pingCount = 0;
-    }
-    
-    ++this.pingId;
-    this.sendFunction('sync_ping', this.pingId, this.getTimeFunction());
-
-    setTimeout(function()  {
-      this$0.__syncLoop();
-    }, 1000 * interval);
-  };
-
-  proto$0.start = function() {
-    this.__syncLoop();
+    this.__syncLoop(sendFunction);
   };
 
   proto$0.getLocalTime = function(syncTime) {
