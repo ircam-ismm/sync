abd6ef2c2ac9152019a8bb452a85c5a121efebf1
jipodine <jipodine@users.noreply.github.com>
2015-03-19 18:57:29 +0100

    Better timeout scheme, and options for constructor

--- client/index.es6.js
+++ client/index.es6.js
@@ -5,30 +5,36 @@ var EventEmitter = require('events').EventEmitter;
 
 class SyncClient extends EventEmitter {
   constructor(getTimeFunction, options = {}) {
-    this.pingTimeoutDelay = options.pingTimeoutDelay || 1; // timeout to consider a ping was not ponged back
+    // timeout to consider a ping was not ponged back
+    this.pingTimeoutDelay = options.pingTimeoutDelay
+      || { min: 1, max: 30 };
+    // number of ping-pongs in a streak
+    this.pingStreakIterations = options.pingStreakIterations || 10;
+    // interval between pings in a streak (in seconds)
+    this.pingStreakPeriod = options.pingStreakPeriod || 0.250;
+    // range of interval between ping-pong streaks (in seconds)
+    this.pingStreakDelay = options.pingStreakDelay
+      || { min: 10, max: 20 };
+    // number of quickest roundtrip times used to compute mean offset
+    this.dataBest = options.keepBestN || 4;
 
-    this.pingStreakIterations = options.pingStreakIterations || 10; // number of ping-pongs in a streak
-    this.pingStreakPeriod = options.pingStreakPeriod || 0.250; // interval between pings in a streak (in seconds)
-    this.pingStreakDelay = options.pingStreakDelay || [10, 20]; // range of interval between ping-pong streaks (in seconds)
     this.pingDelay = 0; // current delay before next ping
     this.pingCount = 0; // elapsed pings in a streak
     this.pingId = 0; // absolute ID to mach pong against
     this.pingTimeoutId = 0; // to cancel timeout on sync_pinc
 
     this.data = []; // circular buffer
-    this.dataNextIndex = 0; // next index in circular buffer
+    this.dataNextIndex = 0; // next index to write in circular buffer
     this.dataLength = this.pingStreakIterations; // size of circular buffer
-    this.dataBest = Math.min(4, this.dataLength); // number of quickest roundtrip times used to compute mean offset
+    this.dataBest = Math.min(this.dataBest, this.dataLength);
 
     this.timeOffset = 0;
     this.travelTime = 0;
     this.travelTimeMax = 0;
 
-    if (this.pingStreakDelay[0] > this.pingStreakDelay[1]) {
-      let tmp = this.pingStreakDelay[0];
-      this.pingStreakDelay[0] = this.pingStreakDelay[1];
-      this.pingStreakDelay[1] = this.pingStreakDelay[0];
-    }
+    orderMinMax(this.PingStreakDelay);
+    orderMinMax(this.pingTimeoutDelay);
+    this.pingTimeoutDelay.current = this.pingTimeoutDelay.min;
 
     this.getTimeFunction = getTimeFunction;
   }
@@ -39,19 +45,29 @@ class SyncClient extends EventEmitter {
     sendFunction('sync_ping', this.pingId, this.getTimeFunction());
 
     this.timeoutId = setTimeout(() => {
-      debug('sync_ping timeout > %s', this.pingTimeoutDelay);
+      // increase timeout duration on timeout, to avoid overflow
+      this.pingTimeoutDelay.current = Math.min(this.pingTimeoutDelay.current * 2,
+                                               this.pingTimeoutDelay.max);
+      debug('sync_ping timeout > %s', this.pingTimeoutDelay.current);
       this.__syncLoop(sendFunction); // retry (yes, always increment pingId)
-    }, 1000 * this.pingTimeoutDelay);
+    }, 1000 * this.pingTimeoutDelay.current);
   }
 
   start(sendFunction, receiveFunction) {
     receiveFunction('sync_pong', (pingId, clientPingTime, serverPingTime, serverPongTime) => {
-      if (pingId === this.pingId) {
+      // accept only the pong that corresponds to the last ping
+      // or accepts anything when lost
+      if (pingId === this.pingId
+          || this.pingTimeoutDelay.current === this.pingTimeoutDelay.max) {
         ++ this.pingCount;
+        debug('pingCount = %s', this.pingCount);
         clearTimeout(this.timeoutId);
+        // reduce timeout duration on pong, for better reactivity
+        this.pingTimeoutDelay.current = Math.max(this.pingTimeoutDelay.current * 0.75,
+                                                 this.pingTimeoutDelay.min);
 
-        var clientPongTime = this.getTimeFunction();
-        var travelTime = Math.max(0, (clientPongTime - clientPingTime) - (serverPongTime - serverPingTime));
+        const clientPongTime = this.getTimeFunction();
+        const travelTime = Math.max(0, (clientPongTime - clientPingTime) - (serverPongTime - serverPingTime));
         const timeOffset = ((serverPingTime - clientPingTime) + (serverPongTime - clientPongTime)) * 0.5;
 
         this.data[this.dataNextIndex] = [travelTime, timeOffset];
@@ -61,8 +77,8 @@ class SyncClient extends EventEmitter {
         if (this.pingCount >= this.pingStreakIterations
             && this.data.length >= this.dataLength) {
           // plan the begining of the next streak
-          this.pingDelay = this.pingStreakDelay[0]
-            + Math.random() * (this.pingStreakDelay[1] - this.pingStreakDelay[0]);
+          this.pingDelay = this.pingStreakDelay.min
+            + Math.random() * (this.pingStreakDelay.max - this.pingStreakDelay.min);
           this.pingCount = 0;
 
           // mean travel time over the last iterations
@@ -90,9 +106,13 @@ class SyncClient extends EventEmitter {
         setTimeout(() => {
           this.__syncLoop(sendFunction);
         }, 1000 * this.pingDelay);
-
-      } // ping and pong ID match
-
+        // ping and pong ID match
+      } else {
+        debug('ping-pong mismatch: %s ≠ %s, pingCount = %s, travelTime = %s, timeOffset = %s',
+              pingId, this.pingId, this.pingCount,
+              Math.max(0, (clientPongTime - clientPingTime) - (serverPongTime - serverPingTime)),
+              ((serverPingTime - clientPingTime) + (serverPongTime - clientPongTime)) * 0.5);
+      }
     }); // receive function
 
     this.__syncLoop(sendFunction);
@@ -109,6 +129,15 @@ class SyncClient extends EventEmitter {
   getSyncTime(localTime = this.getTimeFunction()) {
     return localTime + this.timeOffset; // always convert
   }
+}
+
+function orderMinMax(that) {
+  if(that && that.min && that.max && that.min > that.max) {
+    const tmp = that.min;
+    that.min = that.max;
+    that.max = tmp;
+  }
+  return that;
 }
 
 module.exports = SyncClient;
